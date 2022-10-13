d8891aed9d9dbdcaf22b898e8edf64263add77a2
Michelle Lobo <Michelle Lobo@DESKTOP-NE2EQMN>
2022-10-13 17:19:46 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnsuma_Clicked(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtnum1.Text);
+            int b = Convert.ToInt32(txtnum2.Text);
+            int resultado = a+b;
+            string operador = "suma";
+            await Navigation.PushAsync(new Views.resultado(resultado,operador));
+
+                }
+
+        private async void btnmultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtnum1.Text);
+            int b = Convert.ToInt32(txtnum2.Text);
+            int resultado = a * b;
+            string operador = "multiplicacion";
+            await Navigation.PushAsync(new Views.resultado(resultado, operador));
+
+        }
+
+        private async void btnresta_Clicked(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtnum1.Text);
+            int b = Convert.ToInt32(txtnum2.Text);
+            int resultado = a - b;
+            string operador = "resta";
+            await Navigation.PushAsync(new Views.resultado(resultado, operador));
+
+        }
+
+        private async void btndivision_Clicked(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtnum1.Text);
+            int b = Convert.ToInt32(txtnum2.Text);
+            int resultado = a / b;
+            string operador = "division";
+            await Navigation.PushAsync(new Views.resultado(resultado, operador));
+
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Views/resultado.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class resultado : ContentPage
+    {
+        public resultado(int resultado, string operador)
+        {
+            InitializeComponent();
+            laberesultado.Text = "El resultado de la " + operador + " es: " +resultado;
+        }
+    }
+}
